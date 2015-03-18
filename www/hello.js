09258dece139aed43f79992ab5ccb39f971cd9cf
Pablo Morales <moralespablogaston@gmail.com>
2015-03-18 11:04:17 -0300

    Modificacion del xml para que de permisos de bluetooth

--- www/hello.js
+++ www/hello.js
@@ -1,7 +1,28 @@
 /*global cordova, module*/
 
 module.exports = {
-    greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+    escan: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "escan", [name]);//service= "Hello" // action="greet"
+    },
+    activar: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "activar", [name]);//service= "Hello" // action="greet"
+    },
+    desactivar: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "desactivar", [name]);//service= "Hello" // action="greet"
+    },
+    conectar: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "conectar", [name]);//service= "Hello" // action="greet"
+    },
+    desconectar: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "desconectar", [name]);//service= "Hello" // action="greet"
+    },
+    leer: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "leer", [name]);//service= "Hello" // action="greet"
+    },
+    escribir: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "escribir", [name]);//service= "Hello" // action="greet"
     }
+
+
+
 };
